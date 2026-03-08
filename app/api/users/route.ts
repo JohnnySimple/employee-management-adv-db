c8713e4a66933775d94dd05611e3b39ea0b73111
Rickson Dsouza <ricksond2003@gmail.com>
2026-03-08 16:36:02 -0400

    user-creation-added

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,3 +9,32 @@ export async function GET() {
         return NextResponse.json({error: "Error fetching users"}, {status: 500})
     }
 }
+
+// Post Method
+export async function POST(req:Request){
+    try{
+        const body:{name:string,email:string}=await req.json();
+
+        // Validation
+        if(!body.name || !body.email){
+            return NextResponse.json(
+            {error:"Name and Email Required"},
+            {status:400}
+            );
+        }
+
+        const user= await prisma.user.create({
+            data:{
+                name:body.name,
+                email:body.email
+            }
+        });
+
+        return NextResponse.json(user);
+    } catch(error){
+        return NextResponse.json(
+            {error:"Error Creating User"},
+            {status:500}
+        );
+    }
+}
