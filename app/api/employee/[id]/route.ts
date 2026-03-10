9cce119631cbc806e00c7b45c10519b028624858
Rickson Dsouza <ricksond2003@gmail.com>
2026-03-10 15:55:23 -0400

    Employee-Management-API Updated

--- app/api/employee/[id]/route.ts
+++ app/api/employee/[id]/route.ts
@@ -1,44 +1,70 @@
 import { NextResponse } from "next/server";
 import {prisma} from "@/lib/db"
 
+
+// GET Employee BY ID
+export async function GET({params}:{params:{id:string}}){
+    try{
+        // find Employees
+        const employee=await prisma.employee.findUnique({
+            where :{
+                employeeId:Number(params.id)
+            },
+            include:{
+                department:true,
+                jobTitle:true
+            }
+        });
+
+        if(!employee){
+            return NextResponse.json(
+                {error:"Employee Not Found"},
+                {status:404}
+            );
+        }
+        return NextResponse.json(employee)
+    } catch(error){
+        return NextResponse.json(
+            {error:"Unable to Retreive Employee"},
+            {status:500}
+        );
+    }
+}
+
 // Update employee records
 export async function PUT(req:Request,{params}:{params:{id:string}}){
     try{
         const data=await req.json();
-
-        const {
-            firstName,
-            lastName,
-            dob,
-            hireDate,email,
-            phone,
-            jobStatus,
-            deptId,
-            jobTitleId
-        }=data;
-
-        const employee=await prisma.employee.update({
-            where :{
-                employeeId:Number(params.id)
-            },
-            data:{
-                firstName,
-                lastName,
-                dob,
-                hireDate,
-                email,
-                phone,
-                jobStatus,
-                deptId,
-                jobTitleId 
+        // required Fields
+        const requiredFields=["firstName","lastName","dob","hireDate","email","phone","jobStatus","deptId","jobTitleId"];
+        for(const field of requiredFields){
+            if(!data[field]){
+                return NextResponse.json(
+                    {error:`${field} is required`},
+                    {status:400}
+                );
             }
-        }); 
-
-        return NextResponse.json(employee);
-    } catch(error){
+        }
+        // update Employee
+        const employee=await prisma.employee.update({
+            where:{employeeId:Number(params.id)},
+            data:{
+                firstName:data.firstName,
+                lastName:data.lastName,
+                dob:data.dob,
+                hireDate:data.hireDate,
+                email:data.email,
+                phone:data.phone,
+                jobStatus:data.jobStatus,
+                deptId:data.deptId,
+                jobTitleId:data.jobTitleId
+            }
+        });
+        return NextResponse.json(employee)
+    }catch(error){
         return NextResponse.json(
-            {error: "Unable to Update Employee Information"},
-            {status : 500}
+            {error:"Unable to Update Employee"},
+            {status:500}
         );
     }
 }
@@ -46,18 +72,17 @@ export async function PUT(req:Request,{params}:{params:{id:string}}){
 // Delete Method which soft deletes and not completely.
 export async function PATCH(req:Request,{params}:{params:{id:string}}){
     try{
-        const data=await req.json();
-
+        // Soft Update
         const employee=await prisma.employee.update({
             where : {
                 employeeId :Number(params.id),
             },
             data : {
-                jobStatus:'inactive'
+                jobStatus:'INACTIVE'
             }
         });
 
-        return NextResponse.json(employee)
+        return NextResponse.json({message: "Employee Deactivated Successfully",employee});
     } catch(error){
         return NextResponse.json(
             {error:"Unable to Delete Employee Information"},
