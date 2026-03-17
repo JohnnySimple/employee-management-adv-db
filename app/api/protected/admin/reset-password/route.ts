d55b5e84f7a3cc7fec4ae3906447e7822ac6c9cb
JohnnySimple <obengboatengjohnson@gmail.com>
2026-03-17 10:36:00 -0400

    reset password using employeeId

--- app/api/protected/admin/reset-password/route.ts
+++ app/api/protected/admin/reset-password/route.ts
@@ -4,19 +4,32 @@ import bcrypt from "bcrypt";
 
 export async function POST(req: Request) {
     try {
-        const { userId, newPassword } = await req.json();
+        const { employeeId, newPassword } = await req.json();
 
-        if (!userId || !newPassword) {
+        if (!employeeId || !newPassword) {
             return NextResponse.json(
                 { error: "Missing fields" },
                 { status: 400 }
             )
         }
 
+        const user = await prisma.user.findUnique({
+            where: {
+                employeeId: Number(employeeId)
+            }
+        })
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "User account not found" },
+                { status: 404 }
+            )
+        }
+
         const hashedPassword = await bcrypt.hash(newPassword, 10);
 
         await prisma.user.update({
-            where: { id: Number(userId) },
+            where: { id: user.id },
             data: {
                 passwordHash: hashedPassword
             }
