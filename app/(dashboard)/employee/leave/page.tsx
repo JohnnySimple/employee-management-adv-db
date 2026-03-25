6db4cae310f3bcce61b5fb920fe187bd38da48d6
JohnnySimple <obengboatengjohnson@gmail.com>
2026-03-25 17:12:27 -0400

    add leave history

--- app/(dashboard)/employee/leave/page.tsx
+++ app/(dashboard)/employee/leave/page.tsx
@@ -2,16 +2,83 @@
 
 import { useState, useEffect } from "react";
 import { Card, CardHeader, CardContent, CardTitle, CardDescription } from "@/components/ui/card";
-import { ChartBar } from "lucide-react";
+import { ChartBar, Pill } from "lucide-react";
 import api from "@/lib/api";
+import { TreePalm } from "lucide-react";
+import LeaveHistoryTable from "@/components/employee/leave-history-table";
 
 export default function EmployeeLeave() {
-    
+
+    const [leaveHistory, setLeaveHistory] = useState(null);
+
+    useEffect(() => {
+        const fetchLeaveHistory = async () => {
+            try {
+                const response = await api.get("/leaveemp");
+                setLeaveHistory(response.data);
+            } catch (error) {
+                console.error("Error fetching user leave history:", error);
+            }
+        };
+
+        fetchLeaveHistory();
+    }, [])
+
 
     return (
         <div className="p-6 space-y-6">
+            <h3>Leave Balance</h3>
+            <div className="flex flex-wrap gap-4 mb-6">
+                <div className="w-full sm:w-[48%] lg:w-[23%]">
+                    <Card className="flex-1 rounded-sm">
+                        <CardContent className="p-0">
+                        <div className="px-2 flex flex-row items-center justify-between">
+                            <p className="text-xl font-bold text-purple-600">PTO</p>
+                            <TreePalm className="w-5 h-5 text-muted-foreground" />
+                        </div>
+                        <hr className="my-2 border-slate-200 dark:border-slate-800" />
+                        <div className="px-2">
+                            <p className="text-xl font-bold text-muted-foreground">5/14 days used</p>
+                        </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className="w-full sm:w-[48%] lg:w-[23%]">
+                    <Card className="flex-1 rounded-sm">
+                        <CardContent className="p-0">
+                        <div className="px-2 flex flex-row items-center justify-between">
+                            <p className="text-xl font-bold text-purple-600">Sick Leave</p>
+                            <Pill className="w-5 h-5 text-muted-foreground" />
+                        </div>
+                        <hr className="my-2 border-slate-200 dark:border-slate-800" />
+                        <div className="px-2">
+                            <p className="text-xl font-bold text-muted-foreground">3/10 days used</p>
+                        </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
             <div className="flex flex-col lg:flex-row gap-6 w-full">
-                leave management
+                {/* Time Logging Card */}
+                <div className="flex-1">
+                    <Card className="h-full border-0 shadow-sm">
+                        <CardHeader className="pb-3">
+                        <CardTitle className="text-xl font-semibold flex items-center gap-2">
+                            {/* <Clock className="h-5 w-5 text-primary" /> */}
+                            Leave Request History
+                        </CardTitle>
+                        <CardDescription>
+                            View leave request history
+                        </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-6">
+                        {/* Clock In/Out Buttons */}
+                        {/* <div className="flex gap-3"> */}
+                            <LeaveHistoryTable history={ leaveHistory } />
+                        {/* </div> */}
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     )
---
+++ components/employee/leave-history-table.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { 
+    Table,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableBody,
+    TableRow
+ } from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import { Card } from "../ui/card";
+import { Button } from "../ui/button";
+import { Pencil } from "lucide-react";
+
+
+export default function LeaveHistoryTable({ history }: { any }) {
+
+    return (
+        <Card className="overflow-hidden p-6">
+            <div className="overflow-x-auto">
+                <Table>
+                    <TableHeader className="bg-muted/50">
+                        <TableRow>
+                            <TableHead>LEAVE TYPE</TableHead>
+                            <TableHead>START DATE</TableHead>
+                            <TableHead>END DATE</TableHead>
+                            <TableHead>STATUS</TableHead>
+                            <TableHead>ACTION</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {history?.length === 0 && (
+                            <TableRow>
+                                <TableCell colSpan={4} className="text-center text-muted-foreground">
+                                    No leave history for you now.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                        {history?.map((leave, index) => (
+                            <TableRow key={index}
+                                className={cn("transition-colors hover:bg-muted/50",
+                                    index % 2 === 0 ? "bg-background" : "bg-muted/20"
+                                )}>
+                                <TableCell className="text-sm font-medium">{ leave.leaveType }</TableCell>
+                                <TableCell className="text-sm font-medium">{new Date(leave.startDate).toLocaleDateString(undefined,
+                                    {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric"
+                                    }
+                                )}</TableCell>
+                                <TableCell className="text-sm font-medium">{new Date(leave.endDate).toLocaleDateString(undefined,
+                                    {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric"
+                                    }
+                                )}</TableCell>
+                                <TableCell className="text-sm font-medium">{ leave.status }</TableCell>
+                                <TableCell className="text-sm text-muted-foreground">
+                                    Action
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+        </Card>
+    )
+ }
