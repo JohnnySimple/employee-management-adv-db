137ab9cd684645164b5729aac8eb7c8ea8cab0c5
Rickson Dsouza <ricksond2003@gmail.com>
2026-04-07 12:07:53 -0400

    Updates to Leave Page(Partial)

--- app/api/leave/route.ts
+++ app/api/leave/route.ts
@@ -10,7 +10,7 @@ export async function GET(){
                     include:{
                         employee:true,
                         leave:true
-                    }
+                    },
                 }
             }
         });
