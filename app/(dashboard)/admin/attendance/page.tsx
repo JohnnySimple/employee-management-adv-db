b84c70251f72d812aa0f7fde3f9b40dee5cc93f2
Rickson Dsouza <ricksond2003@gmail.com>
2026-04-08 15:44:13 -0400

    Added Leave Page Metadata

--- app/(dashboard)/admin/attendance/page.tsx
+++ app/(dashboard)/admin/attendance/page.tsx
@@ -1,4 +1,10 @@
 import AdminLeavePage from "@/components/admin/admin-leave-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "EMS | Admin Leave Management",
+    description: "Admin dashboard for managing employee leave requests."
+}
 
 export default function AdminPage(){
     return (
